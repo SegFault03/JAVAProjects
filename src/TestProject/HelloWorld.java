4c285c4f2e1e08b70d1944efd2dc4be153df0abb
Niladri Das <boney0310@gmail.com>
2022-09-05 01:27:30 +0530

    intial commit

---
+++ src/TestProject/HelloWorld.java
@@ -0,0 +1,6 @@
+package TestProject;
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/TestProject/Inheritance/SuperClass.java
@@ -0,0 +1,57 @@
+package TestProject.Inheritance;
+
+public class SuperClass {
+    private int a,b;
+    public static final String CLASS_NAME ="SuperClass";
+
+    /**
+     * SuperClass constructor
+     * @param a
+     * @param b
+     */
+    SuperClass(int a, int b)
+    {
+        this.a=a;
+        this.b=b;
+    }
+
+    /**
+     * displays value of a and b
+     */
+    public void display()
+    {
+        System.out.println("a= "+a+"\n"+"b= "+b);
+    }
+
+    /**
+     * displays a test string
+     */
+    public void callUsingSubClassObject()
+    {
+        System.out.println("This is called using the sub class object");
+    }
+
+    /**
+     * static object for displaying class name
+     */
+    public static void displayClassName()
+    {
+        System.out.println("Class name = "+ CLASS_NAME);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
